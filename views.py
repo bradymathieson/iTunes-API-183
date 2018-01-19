5db73c01c887ebbb2bb013457dbdfe11a06eebf0
Brady Mathieson <bradmath@umich.edu>
2018-01-19 13:17:57 -0500

    initial

---
+++ views.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('controllers')
+
+'''
+Add your pages down here as imported modules from the /controllers directory. Each
+page should have an import statement. This is how we are connecting our controllers 
+to our website!
+
+For example, if your page is named file.py, write:
+	from file import *
+'''
+
+from index import *
